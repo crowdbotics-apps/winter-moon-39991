0be1e099c78f8ba19fb206d04792ef82e64a2452
Crowdbotics <team@crowdbotics.com>
2023-04-17 23:06:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-moon-39991",
+  url: "https://winter-moon-39991.botics.co",
+  api: "https://winter-moon-39991.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
